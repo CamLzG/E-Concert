70674e89fd951a749f0f64998e2b4c7708b81d1d
Camila Lopez <c.lopezgiornes@gmail.com>
2023-04-16 16:03:48 -0300

    tercera entrega

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const cantidadMaxima = 4
+let blurFondo = document.getElementById("blur")
 
-function Entrada(nombre, precio, precioMenores) {
+function Entrada(id, nombre, precio, precioMenores) {
+    this.Id = id;
     this.Nombre = nombre;
     this.Precio = precio;
     this.PrecioMenores = precioMenores;
 }
 
+function ItemCarrito(nombre, entradas, menores, total) {
+    this.Nombre = nombre;
+    this.Entrada = entradas;
+    this.Menores = menores;
+    this.Total = total;
+}
+
 let entradas = [
-    new Entrada("Bad Bunny", 100, 50),
-    new Entrada("Dua Lipa", 200, 100),
-    new Entrada("Metallica", 200, 100),
+    new Entrada("badBunny", "Bad Bunny", 100, 50),
+    new Entrada("duaLipa", "Dua Lipa", 200, 100),
+    new Entrada("metallica", "Metallica", 200, 100),
 ]
 
-let carrito = [];
+let carritoStorage = JSON.parse(localStorage.getItem("carrito"));
+let carrito = carritoStorage != null ? carritoStorage : [];
 
 function calcularPrecio(cantidadEntradas, entradasMenores, entrada) {
     let totalMayores = (cantidadEntradas - entradasMenores) * entrada.Precio
@@ -20,57 +30,95 @@ function calcularPrecio(cantidadEntradas, entradasMenores, entrada) {
     return totalMayores + totalMenores;
 }
 
-alert("Bienvenido al simulador de compra de entradas para conciertos en Uruguay.")
-let artistas = entradas.map(entrada => entrada.Nombre).join(", ");
-do {
-    do {
-        let entradaSeleccionada;
-        let artistaValido = false;
-        alert(`Los conciertos disponibles actualmente son: ${artistas}`);
-        do {
-            let entradaSeleccionadaString = prompt("Escriba el artista que desea ver");
-            artistaValido = entradas.some(entrada => entrada.Nombre.toLowerCase() === entradaSeleccionadaString.toLowerCase());
 
-            if (!artistaValido) {
-                alert(`Artista invalido. Los conciertos disponibles actualmente son: ${artistas}`);
-            }
-            entradaSeleccionada = entradas.find(entrada => entrada.Nombre.toLowerCase() === entradaSeleccionadaString.toLowerCase());
+let registro = document.getElementById("registro")
+registro.addEventListener("click", () => {
+    let formNoticias = document.getElementById("formNoticias")
+    formNoticias.scrollIntoView()
+})
 
-        } while (!artistaValido);
+let artista;
 
-        let cantidadEntradas = 0;
-        let entradasMenores = 0;
+let seleccionConcierto = document.getElementsByClassName("mostrarEntradas")
+for (let index = 0; index < seleccionConcierto.length; index++) {
+    seleccionConcierto[index].addEventListener("click", () => {
+        artista = entradas.find(entrada => entrada.Id === seleccionConcierto[index].id)
+        let seleccionEntradas = document.getElementById("seleccionEntradas")
+        let artistaSelecionado = document.getElementById("artistaElegido")
+        artistaSelecionado.innerHTML = `Has seleccionado a ${artista.Nombre} :`
+        seleccionEntradas.classList.remove("hidden")
+    })
+}
 
-        do {
-            cantidadEntradas = Number(prompt("El precio para mayores es " + entradaSeleccionada.Precio + ". Y el precio para menores es " + entradaSeleccionada.PrecioMenores + " \n Ingresa la cantidad de entradas que deseas (Máximo " + cantidadMaxima + ")"))
-            if (isNaN(cantidadEntradas)) {
-                alert("Debes ingresar un número")
-            } else if (cantidadEntradas > cantidadMaxima) {
-                alert("Solo se permiten " + cantidadMaxima + " entradas por persona")
-            }
+let formularioEntradas = document.getElementById("formularioEntradas")
+formularioEntradas.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let entradasTotales = formularioEntradas.cantidadEntradas.value
+    let entradasMenores = formularioEntradas.entradasMenores.value
+    let total = calcularPrecio(entradasTotales, entradasMenores, artista)
+    carrito.push(new ItemCarrito(artista.Nombre, entradasTotales, entradasMenores, total))
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    formularioEntradas.cantidadEntradas.value = 1
+    formularioEntradas.entradasMenores.value = 0
+    let contendorMsg = document.getElementById("añadido")
+    contendorMsg.innerHTML = `Se ha añadido la/las entrada/s para ${artista.Nombre} al carrito`
+    contendorMsg.classList.toggle("hidden")
+    setTimeout(function () {
+        contendorMsg.classList.toggle("hidden");
+    }, 1000);
+})
 
-        } while (isNaN(cantidadEntradas) || cantidadEntradas > cantidadMaxima || cantidadEntradas < 1);
+let carrito2 = document.getElementById("carritoImg")
+carrito2.addEventListener("click", () => {
+    blurFondo.classList.remove("hidden")
+    let contenedorCarrito = document.getElementById("contenedorCarrito")
+    for (let index = 0; index < carrito.length; index++) {
+        let contenido = document.createElement("p")
+        contenido.innerHTML = `${carrito[index].Nombre} - ${carrito[index].Entrada} Entradas - ${carrito[index].Menores} Menores`
+        contenedorCarrito.prepend(contenido)
+    }
+    contenedorCarrito.classList.remove("hidden")
+})
 
-        if (cantidadEntradas > 1) {
-            do {
-                entradasMenores = Number(prompt("¿Cuantas entradas son para menores?"))
-                if (isNaN(entradasMenores)) {
-                    alert("Debes ingresar un número")
-                } else if (entradasMenores > (cantidadEntradas - 1)) {
-                    alert("Los menores deben ir acompañados de por lo menos 1 adulto.")
-                }
-            } while (isNaN(entradasMenores) || entradasMenores > (cantidadEntradas - 1));
-        }
+let botonCarrito = document.getElementById("calculoTotal")
+botonCarrito.addEventListener("click", () => {
+    let calcularTotal = carrito.reduce((acc, item) => acc + item.Total, 0)
+    let totalCarrito = document.getElementById("totalCarrito")
+    totalCarrito.classList.remove("hidden")
+    let total = document.createElement("p")
+    total.innerHTML = `El total de tu carrito es ${calcularTotal} pesos uruguayos.`
+    totalCarrito.prepend(total)
+    totalCarrito.className = "divEmergente"
+    let sectionCarrito = document.getElementById("contenedorCarrito")
+    sectionCarrito.classList.add("hidden")
+})
 
-        let costoTotal = calcularPrecio(cantidadEntradas, entradasMenores, entradaSeleccionada);
+let reset = document.getElementById("reset")
+reset.addEventListener("click", () => {
+    blurFondo.classList.add("hidden")
+    localStorage.clear()
+    carrito = [];
+    let totalCarrito = document.getElementById("totalCarrito")
+    totalCarrito.classList.add("hidden")
+    location.reload()
+})
 
-        alert("El total sería " + costoTotal + " pesos uruguayos")
 
-        carrito.push(costoTotal);
+let botonRegistro = document.getElementById("formRegistro")
+botonRegistro.addEventListener("submit", (e) => {
+    e.preventDefault()
+    blurFondo.classList.remove("hidden")
+    let formNoticias = document.getElementById("registroCompleto")
+    formNoticias.classList.remove("hidden")
+    let alerta = document.createElement("p")
+    alerta.innerHTML = `¡Tu registro se completó con exito!`
+    formNoticias.prepend(alerta)
+})
 
-    } while (prompt("¿Desea seguir comprando? \n - Si \n - No").toLowerCase() == "si");
-
-    console.log("carrito: ", carrito);
-    alert("El total del carrito sería " + carrito.reduce((a, b) => a + b, 0) + " pesos uruguayos")
-
-} while (prompt("¿Quieres realizar otra simulación? \n - Si \n - No").toLowerCase() == "si");
+let resetForm = document.getElementById("resetForm")
+resetForm.addEventListener("click", () => {
+    blurFondo.classList.add("hidden")
+    location.reload()
+    let formNoticias = document.getElementById("registroCompleto")
+    formNoticias.classList.add("hidden")
+})
